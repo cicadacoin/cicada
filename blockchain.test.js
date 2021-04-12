87eef677680aae6a4491611d86292c1bc877a832
cicadacoin <82412898+cicadacoin@users.noreply.github.com>
2021-04-12 22:02:10 +0200

    Add files via upload

---
+++ blockchain.test.js
@@ -0,0 +1,47 @@
+const Blockchain = require('./blockchain');
+const Block = require('./block');
+
+describe('Blockchain', () => {
+    const blockchain = new Blockchain();
+
+    it('contains a `chain` Array instance', () => {
+        expect(blockchain.chain instanceof Array).toBe(true);
+    });
+
+    it('starts with the genesis block', () => {
+        expect(blockchain.chain[0]).toEqual(Block.genesis());
+    });
+
+    it('adds a new block to the chain', () => {
+        const newData = 'foo bar';
+        blockchain.addBlock({ data: newData });
+
+        expect(blockchain.chain[blockchain.chain.length-1].data).toEqual(newData);
+    });
+
+    describe('isValidChain', () => {
+        describe('when the chain does not start with the genesis block', () => {
+            it('returns false', () => {
+                blockchain.chain[0] = { data: 'fake-genesis' };
+
+                expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+            });
+        });
+
+        describe('when the chain starts with the genesis block and has multiple blocks', () => {
+            describe('and a lastHash reference has changed', () => {
+                it('returns false', () => {});
+            });
+
+            describe('and the chain contains a block wuth an invalid field', () => {
+                it('returns false', () => {});
+            });
+
+            describe('and the chain does not contain any invalid blocks', () => {
+                it('returns true', () => {
+
+                });
+            });
+        });
+    });
+});
